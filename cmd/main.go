7e97f7fb19bbee81f7bbb753af6e6e97618c3051
helouazizi <ouazizi2code@gmail.com>
2025-03-28 17:09:30 +0000

    add server

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+    "log"
+    "net/http"
+    "in-memory-api/api"
+    "in-memory-api/handlers"
+    "in-memory-api/store"
+)
+
+func main() {
+    store := store.NewMemoryStore()
+    store.LoadData() // Fetch API data
+
+    artistHandler := &handlers.ArtistHandler{Store: store}
+
+    api.RegisterRoutes(artistHandler)
+
+    log.Println("Server running on :8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
