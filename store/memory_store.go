29422d4d73d7bcd2fb8f24c71fb8ac44dbd34fe2
helouazizi <ouazizi2code@gmail.com>
2025-03-28 16:47:50 +0000

    add in memory storage

--- store/memory_store.go
+++ store/memory_store.go
@@ -1,1 +1,18 @@
 package store
+
+import (
+	"in-memory-api/models"
+	"sync"
+)
+
+type Memory_Store struct {
+	Artists   []models.Artist
+	Loactions []models.Location
+	Dates     []models.Date
+	Relations []models.Relation
+	Mutex     sync.Mutex
+}
+
+func NewMemoryStore() *Memory_Store {
+	return &Memory_Store{}
+}
