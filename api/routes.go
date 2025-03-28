3ed7d99e423f375d67b1ae26c3fefdb3dba0ade9
helouazizi <ouazizi2code@gmail.com>
2025-03-28 17:08:30 +0000

    add routs

--- api/routes.go
+++ api/routes.go
@@ -1,1 +1,10 @@
 package api
+
+import (
+	"in-memory-api/handlers"
+	"net/http"
+)
+
+func RegisterRoutes(artistHandler *handlers.ArtistHandler) {
+	http.HandleFunc("/artists", artistHandler.GetArtists)
+}
--- handlers/artists.go
+++ handlers/artists.go
@@ -1,1 +1,16 @@
 package handlers
+
+import (
+	"encoding/json"
+	"in-memory-api/store"
+	"net/http"
+)
+
+type ArtistHandler struct {
+	Store *store.Memory_Store
+}
+
+func (h *ArtistHandler) GetArtists(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(h.Store.Artists)
+}
