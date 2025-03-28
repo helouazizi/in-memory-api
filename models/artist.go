9d438a9b16f9394cb29a58cc2248b54444ce5495
helouazizi <ouazizi2code@gmail.com>
2025-03-28 16:51:52 +0000

    modify artist

--- models/artist.go
+++ models/artist.go
@@ -2,12 +2,12 @@ package models
 
 type Artist struct {
 	ID           int      `json:"id"`
-	Image        string   `json:"image"`
+	Image        string   `json:"image"` // api
 	Name         string   `json:"name"`
 	Members      []string `json:"members"`
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
+	Locations    string   `json:"locations"`    // api
+	ConcertDates string   `json:"concertDates"` // api
+	Relations    string   `json:"relations"`    // api
 }
